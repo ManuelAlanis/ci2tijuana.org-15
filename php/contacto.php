e6b75909eced4c18a0703a8de696d0c1cbcf7061
manuelalanis <alanism@uabc.edu.mx>
2015-09-19 13:15:33 -0700

    extenso, actividades, iconos, link aristas

---
+++ php/contacto.php
@@ -0,0 +1,43 @@
+<?php
+
+$EmailFrom = $_REQUEST['email']; 
+//$EmailTo = "ci2t@uabc.edu.mx,"."cgaxiola@uabc.edu.mx,"."dtrujillotoledo@uabc.edu.mx,"."alanism@uabc.edu.mx"; // Your email address here
+$EmailTo ="alanism@uabc.edu.mx";
+$Subject = "Formulario Contacto Ci2tijuana.org";
+$Name = Trim(stripslashes($_POST['name'])); 
+$Email = Trim(stripslashes($_POST['email'])); 
+$Message = Trim(stripslashes($_POST['message'])); 
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  echo "Error";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "Nombre: ";
+$Body .= $Name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $Email;
+$Body .= "\n";
+$Body .= "Mensaje: ";
+$Body .= "\n";
+$Body .= "\n";
+$Body .= $Message;
+$Body .= "\n";
+
+// send email 
+$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+
+// redirect to success page 
+if ($success){
+ header("Location: http://ci2tijuana.org/sen.html");
+  // echo "Succes";
+}
+else{
+  echo "Error";
+}
+?>
